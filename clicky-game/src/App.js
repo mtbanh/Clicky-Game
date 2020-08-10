aa31527de3b644300dc4d7d973a81baac3d0bfe8
Mai Banh <65269951+mtbanh@users.noreply.github.com>
2020-08-10 16:01:52 -0700

    created state and class

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Navbar from "./components/Navbar/Navbar";
+import Card from "./components/Card/Card";
+import Wrapper from "./components/Wrapper/Wrapper";
+import images from "./images.json"
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    images,
+    score: 0,
+    highScore: 0,
+    clicked: [],
+    text: "Click Away!"
+  };
+
+  clickMe = id =>{
+    if(this.state.clicked.includes(id)){
+      this.setState({text: "Sorry. Try again"})
+
+      if(this.state.highScore < this.state.score){
+        this.setState({highScore: this.state.score})
+      };
+
+      this.setState({clicked: []})
+      this.setState({score: 0})
+    }
+
+    else{
+      this.setState({text:"You get another donut!"})
+      this.setState({score: this.state.score +1})
+      this.setState({clicked: [...this.state.clicked, id]})
+    };
+    const newClicked = this.state.images
+  }
 }
 
 export default App;
