e020a630114af54cb012f074db0256f1b62acc78
Mai Banh <65269951+mtbanh@users.noreply.github.com>
2020-08-10 22:04:51 -0700

    Updates

---
+++ precache-manifest.4740738c0e1038e2ffd57ba757588b28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7401a742ed696bb17c7a9b32f73cdb9",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "551051a68fc4b14c10de",
+    "url": "/Clicky-Game/static/css/main.b55caaab.chunk.css"
+  },
+  {
+    "revision": "3f53110666c79e15ed81",
+    "url": "/Clicky-Game/static/js/2.220c254d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Clicky-Game/static/js/2.220c254d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "551051a68fc4b14c10de",
+    "url": "/Clicky-Game/static/js/main.0cc27e9e.chunk.js"
+  },
+  {
+    "revision": "f70336978153cff784ba",
+    "url": "/Clicky-Game/static/js/runtime-main.a0ce3ec2.js"
+  }
+]);
